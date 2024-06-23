812187599dfa9aa08cafc0669890c8b395ff22e2
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-23 07:57:44 +0300

    Добавьте файлы проекта.

---
+++ PrscticeWork6/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace PrscticeWork6
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public double Rating { get; set; }
+    }
+}
---
+++ PrscticeWork6/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace PrscticeWork6
+{
+    public partial class MainWindow : Window
+    {
+        public List<Game> Games { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Games = new List<Game>
+            {
+                new Game { Name = "Ведьмак 3", Genre = "Action", Year = 2015, Rating = 4.8 },
+                new Game { Name = "GTA V", Genre = "Action", Year = 2013, Rating = 4.5 },
+                new Game { Name = "Valorant", Genre = "Shooter", Year = 2020, Rating = 4.7 },
+                new Game { Name = "Genshin Impact", Genre = "MMO RPG", Year = 2020, Rating = 4.5 },
+                new Game { Name = "Dota 2", Genre = "kakashka", Year = 2010, Rating = -2 }
+            };
+
+            gameList.ItemsSource = Games;
+        }
+
+        private void gameList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (gameList.SelectedItem is Game selectedGame)
+            {
+                nameTextBox.Text = selectedGame.Name;
+                genreTextBox.Text = selectedGame.Genre;
+                yearTextBox.Text = selectedGame.Year.ToString();
+                ratingTextBox.Text = selectedGame.Rating.ToString();
+            }
+        }
+        
+    }
+}
